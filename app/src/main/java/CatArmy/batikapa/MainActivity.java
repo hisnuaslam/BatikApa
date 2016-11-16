83f457f146614d4b5ac8ee6e759cb4131b8c3ccb
hisnuaslam <hisnuaslam@gmail.com>
2016-11-16 10:52:51 +0700

    Our new project! "BatikApa"
    First Launch on Github.. Just create a blank activity..
    Still trying to build camera on app
    I'll do my best!
    WIsh me luck, Guys!!

---
+++ app/src/main/java/CatArmy/batikapa/MainActivity.java
@@ -0,0 +1,13 @@
+package CatArmy.batikapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
